614ce6bcc4d7f1c70a51986faf6545dce92cc683
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2025-02-10 22:03:45 +0200

    fix email field in user

--- users/models.py
+++ users/models.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from common.models import BaseModelMixin
 
 
 class User(AbstractUser, BaseModelMixin):
-    email = models.EmailField(unique=True)
 
     class Meta:
         verbose_name = "User"
