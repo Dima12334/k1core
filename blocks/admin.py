11ef7c43383b5599806db32c54815e7aa5feeed2
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2025-02-10 21:49:41 +0200

    fix N+1 in admin

--- blocks/admin.py
+++ blocks/admin.py
@@ -11,3 +11,6 @@ class BlockAdmin(admin.ModelAdmin):
         "number",
     )
     readonly_fields = list_display + ("blockchain_created_at",)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("currency", "provider")
