d081bdecf66db4ff7e585a544eb608462c6e551e
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2025-02-11 17:30:52 +0200

    Block number fixes

--- blocks/models.py
+++ blocks/models.py
@@ -7,13 +7,14 @@ from providers.models import Provider
 class Block(BaseModelMixin):
     currency = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name="blocks")
     provider = models.ForeignKey(Provider, on_delete=models.PROTECT, related_name="blocks")
-    number = models.PositiveIntegerField(unique=True)
+    number = models.PositiveIntegerField()
     best_block_time = models.DateTimeField(null=True, default=None)
 
     class Meta:
         verbose_name = "Block"
         verbose_name_plural = "Blocks"
         db_table = "blocks"
+        unique_together = ("currency", "number")
 
     def __str__(self):
         return f"{self.currency.name}: {self.number}"
